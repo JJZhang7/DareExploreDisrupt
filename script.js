42bf28df87bc8bcedb0e56eb5966815039d848aa
JJZhang7 <43829094+JJZhang7@users.noreply.github.com>
2019-01-09 16:36:43 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".A").click(function(){
+    $(".C").show();
+    $(".A").hide();
+    $(".B").show();
+});
+$(".B").click(function(){
+    $(".B").hide();
+    $(".D").show();
+    $(".C").val();
+    
+});
